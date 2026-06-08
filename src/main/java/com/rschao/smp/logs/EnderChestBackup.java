99d4253ebd282a77ac78e4d453f01ece4dadb438
RSChao <rschao@outlook.com>
2026-06-08 20:33:32 +0200

    change axvaults backup to only happen once a month (otherwise, server storage would die)

--- src/main/java/com/rschao/smp/logs/EnderChestBackup.java
+++ src/main/java/com/rschao/smp/logs/EnderChestBackup.java
@@ -6,6 +6,7 @@ import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.Instant;
 
 public class EnderChestBackup {
     //java.time.Instant lastBackup = java.time.Instant.now();
@@ -18,6 +19,15 @@ public class EnderChestBackup {
         if(!file.exists()){
             return;
         }
+
+        Instant lastBackup = getLastBackupDate();
+        if(lastBackup != null && java.time.Duration.between(lastBackup, java.time.Instant.now()).toDays() < 28){
+            System.out.println("Last backup was less than 28 days ago, skipping backup");
+            return;
+        }
+
+        saveBackupDate();
+
         File backupFolder = new File(com.rschao.smp.Plugin.getPlugin(com.rschao.smp.Plugin.class).getDataFolder(), "backups/" + java.time.Instant.now().toString().replace(":", "_")+"/");
         if(!backupFolder.exists()){
             backupFolder.mkdirs();
@@ -53,6 +63,30 @@ public class EnderChestBackup {
             System.out.println("Restored " + backupFolder.listFiles().length + " enderchests from " + backupFolder.getAbsolutePath());
         } catch (java.io.IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void saveBackupDate(){
+        //save the date of the last backup to a file
+        File file = new File(com.rschao.smp.Plugin.getPlugin(com.rschao.smp.Plugin.class).getDataFolder(), "last_backup.txt");
+        try {
+            java.nio.file.Files.write(file.toPath(), java.time.Instant.now().toString().getBytes());
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Instant getLastBackupDate(){
+        File file = new File(com.rschao.smp.Plugin.getPlugin(com.rschao.smp.Plugin.class).getDataFolder(), "last_backup.txt");
+        if(!file.exists()){
+            return null;
+        }
+        try {
+            String date = new String(java.nio.file.Files.readAllBytes(file.toPath()));
+            return Instant.parse(date);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
